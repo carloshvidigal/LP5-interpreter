309a35f5713e10b5904f5b435c488e9e904f6f2d
Carlos Henrique Vidigal <82422474+carloshvidigal@users.noreply.github.com>
2023-05-27 11:39:40 -0300

    Create main classes

---
+++ src/main/java/interpreter/IMC.java
@@ -0,0 +1,14 @@
+package interpreter;
+
+public class IMC {
+    public static String formula = "peso / altura / altura";
+
+    public static double calcularIMC(double peso, double altura) {
+        String expressao;
+        expressao = formula.replace("peso", Double.toString(peso));
+        expressao = expressao.replace("altura", Double.toString(altura));
+        InterpretadorExpressao interpretador = new InterpretadorExpressoesAritmeticas(expressao);
+        return interpretador.interpretar();
+
+    }
+}
